e2394c09556f1a0565a2dda4a79cd9c813e226a7
rohitraj1845 <133978902+rohitraj1845@users.noreply.github.com>
2023-05-19 01:53:48 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,91 @@
+QUESTION:-1. How do you comment code in python? what are the different types of comments?
+# python is simple and easy to understand.
+"""python is a dynamically typed, general purpose programming language that supports an object-oriented programming approach as well as a functional programming approach."""
+There are two types of comments:-
+(i) Single line comment.(#)
+(ii) Multi-line comment.(""" """ or ''' ''')
+
+
+QUESTION:-2. What are variables in python? How do you declare and assign values to variables?
+-->A variables is the name given to a memory location in a program.
+Declare variables in a python:-
+variable_name = value
+for example:-
+a = 123
+print(a)
+b = 234.123
+print(b)
+c ="Rohit"
+print(c)
+
+
+
+QUESTION:-3. How do convert one data type to another in python?
+a = 123456
+print(type(a))
+a = str(123456)
+print(type(a))
+a = float(123456)
+print(type(a))
+a = bool(123456)
+print(type(a))
+a = complex(123456)
+print(type(a))
+
+
+
+
+Question:-4. How do you write and execute a python script from the command line?
+-->Open a command-line and type in the word python, or python3 if you have both versions, followed by the path to your script.
+
+
+
+QUESTION:-5. Given a list my_list = [1,2,3,4,5], write the code to slice the list and obtain the sub-list [2,3].
+my_list = [1,2,3,4,5]
+print(my_list)
+print(my_list[1:3])
+
+
+
+
+
+QUESTION:-6. What is a complex number in mathematics, and how is it represented in python?
+--> The numbers that are expressed in the form of a+ib where, a,b are real numbers and 'i' is an imaginary number.
+Complex number represented in python:-
+The numbers that are expressed in the form of a+bj where, a,b are real numbers and 'i' is an imaginary number.
+for example:- 2+3j
+
+
+
+
+QUESTION:-7. What is the correct way to declare a variable named age and assign the value 25 to it?
+age = 25
+print("The age is : ",age)
+
+
+
+QUESTION:-8. Declare a variable named price and assign the value 9.99 to it. What data type does this variable belong to?
+price = 9.99
+print(type(price))
+
+
+
+
+QUESTION:-9. Create a variable named name and assign your full name to it as a string.How would you print the value of this variable?
+name = "Rohit Raj"
+print(name)
+
+
+
+QUESTION:-10. Given the string "Hello, World!", extract the substring"World".
+a = "Hello, World!"
+print(a[6:12])
+
+
+
+QUESTION:-11. Create a variable named "is_student" and assign it a boolean value indicating wheather you are currently a student or not.
+is_student = True
+if(is_student == 1): 
+    print("a student")
+else: 
+    print("not student")    
